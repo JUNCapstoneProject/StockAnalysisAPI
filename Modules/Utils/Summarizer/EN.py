ff4085a8bcb5f5c9903c83f762a8fa01e6caf137
BlackChihiro <blackayano00@gmail.com>
2025-06-02 23:11:44 +0900

    최소 입력 길이 하향

--- Modules/Utils/Summarizer/EN.py
+++ Modules/Utils/Summarizer/EN.py
@@ -1,4 +1,3 @@
-from torch.utils.data import Dataset
 from transformers import pipeline
 
 
@@ -9,30 +8,19 @@ class ENSummarizer:
         self.summarizer = pipeline('summarization', model=self.MODEL)
 
     def summarize(self, text):
-        outs = []  # outs 리스트를 while 루프 외부에서 선언
+        outs = []
 
         while len(text) > 1024:
-            chunk_text = self.get_overlapped_chunks(text, 1024, 64)
-            for out in self.summarizer(ListDataset(chunk_text),
-                                       batch_size=4, max_length=128, min_length=32):
-                outs.append(out[0]['summary_text'])
-            text = '\n'.join(outs)  # 새로 생성된 'outs'로 다시 text를 업데이트
+            chunk_text = self.get_overlapped_chunks(text, 1024, 64)  # 이미 List[str] 형태여야 함
+            for out in self.summarizer(chunk_text,
+                                       max_length=128, min_length=32):
+                outs.append(out['summary_text'])  # 'out'은 dict
+            text = '\n'.join(outs)
 
-        # 최종 요약 텍스트 생성
-        summarized_text = self.summarizer('\n'.join(outs), max_length=128, min_length=16)[0]['summary_text']
+        # 최종 요약
+        summarized_text = self.summarizer(text, max_length=128, min_length=16)[0]['summary_text']
         return summarized_text
 
     @staticmethod
     def get_overlapped_chunks(text, chunk, overlap):
         return [text[a:a + chunk] for a in range(0, len(text), chunk - overlap)]
-
-
-class ListDataset(Dataset):
-    def __init__(self, dataset):
-        self.dataset = dataset
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, i):
-        return self.dataset[i]
